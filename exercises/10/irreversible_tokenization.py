1f0ed7197c3eac9256f4e120c7acc63b7d2db350
Michael Senn <michael@morrolan.ch>
2021-12-02 13:19:07 +0100

    Exercise 10: Implement Luhn checksum and cycle-walking algorithm.

---
+++ exercises/10/irreversible_tokenization.py
@@ -0,0 +1,107 @@
+import hashlib
+
+def calculate_luhn_checksum(s):
+    '''Calculates the Luhn checksum of the number encoded in s
+
+    Input:
+      s: String representation of base-10 number of which to calculate Luhn
+         checksum
+
+    Returns:
+      int: Luhn checkdigit
+
+    Raises:
+      ArgumentError: If input is not a number
+    '''
+
+    try:
+        int(s)
+    except ValueError:
+        raise ValueError(f"Passed value was not a valid base-10 number: {s}")
+
+    digits = [int(c) for c in s]
+    # We'll revert them here, then we can just iterate forward
+    digits.reverse()
+
+    sum = 0
+    for idx, digit in enumerate(digits):
+        if idx % 2 == 0:
+            sum += 2 * digit
+            if digit >= 5:
+                # If digit * 2 >= 10, we actually wanted to add its cross sum
+                # As 0 <= digit <= 9, we know that 0 <= 2 * digit <= 18
+                # For a number x in [10, 18], its crosssum is equal to x - 9
+                sum -= 9
+
+        else:
+            sum += digit
+
+    return (10 - sum) % 10
+
+def luhn(s):
+    '''Validates a Luhn checksum
+
+    The check digit is assumed to be in the last position.
+
+    Input:
+      s: String representation of base-10 number which to verify.
+
+    Raises:
+      ArgumentError: If input is not a number, or too short.
+
+    Returns:
+      bool: Indicating whether input's last digit was a valid Luhn checksum
+    '''
+    if len(s) < 2:
+        raise ValueError(f"Passed value was too short: {s}")
+
+    try:
+        int(s)
+    except ValueError:
+        raise ValueError(f"Passed value was not a valid base-10 number: {s}")
+
+    # We'll assume that the checksum digit is the last digit
+    digit_str, checksum = s[0:-1], s[-1]
+    actual_checksum = calculate_luhn_checksum(digit_str)
+
+    return actual_checksum == int(checksum)
+
+def hash_token(s):
+    '''Generates a new valid Luhn-checksummed number using a one-way hash function
+
+    Generation of a new number happens by means of the cycle-walking algorithm,
+    with the SHA256 hash function.
+
+    Input:
+      s: String representation of base-10 number to use as starting point
+
+    Raises:
+      ValueError: If input was not a valid Luhn-checksummed number
+
+    Returns:
+      str: Base-10 representation of a new Luhn-checksummed sequence
+    '''
+    # Raise unless valid Luhn checksum
+    if not luhn(s):
+        raise ValueError(f"Input was not a valid Luhn-checksummed number: {s}")
+
+    # Input is numeric, so ASCII is a sane enough choice
+    input_bytes = s.encode('ascii')
+    n = len(s)
+
+    while True:
+        input_bytes = hashlib.sha256(input_bytes).digest()
+
+        # Interpret digest bytes as big-endian integer, reduce to an appropriate length
+        # This is safe as SHA256's output is (conjectured to be)
+        # indistinguishable from uniform random.
+        candidate = str(int.from_bytes(input_bytes, "big") % 10**n)
+
+        # If we got a valid Luhn-checksumed number, return it. Else continue
+        # trying.
+        if luhn(candidate):
+            return candidate
+
+if __name__ == '__main__':
+    out = irreversible_tokenization("7992739871")
+    print(out)
---
+++ exercises/10/irreversible_tokenization_test.py
@@ -0,0 +1,48 @@
+import unittest
+import irreversible_tokenization as it
+
+class TestIrreversibleTokenization(unittest.TestCase):
+    def test_hash_token(self):
+        next_token = it.hash_token("79927398713")
+        self.assertTrue(it.luhn(str(next_token)))
+
+    def test_hash_token_raises_on_invalid_input(self):
+        s = "79927398712"
+        with self.assertRaises(ValueError):
+            it.hash_token(s)
+
+
+class TestLuhnChecksum(unittest.TestCase):
+    def test_calculation(self):
+        s = "7992739871"
+        checksum = it.calculate_luhn_checksum(s)
+        self.assertEqual(checksum, 3)
+
+        s = "13482517501235"
+        checksum = it.calculate_luhn_checksum(s)
+        self.assertEqual(checksum, 9)
+
+    def test_calculation_raises_on_invalid_number(self):
+        s = "79helloworld"
+        with self.assertRaises(ValueError):
+            it.calculate_luhn_checksum(s)
+
+    def test_verification(self):
+        s = "79927398713"
+        self.assertTrue(it.luhn(s))
+
+        s = "79927398714"
+        self.assertFalse(it.luhn(s))
+
+    def test_verification_raises_on_invalid_number(self):
+        s = "7992739871x"
+        with self.assertRaises(ValueError):
+            it.luhn(s)
+
+    def test_validation_raises_on_too_short_number(self):
+        s = "3"
+        with self.assertRaises(ValueError):
+            it.luhn(s)
+
+if __name__ == '__main__':
+    unittest.main()
